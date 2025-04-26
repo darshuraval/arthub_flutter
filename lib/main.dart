c572900f00417d755ff48f20ef1fcb8d22b60998
Darshan Raval <110303703+darshuraval@users.noreply.github.com>
2025-04-26 15:28:26 +0530

    created lib1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:arthub_flutter/config/app_styles.dart';
-import 'package:arthub_flutter/screens/main_screen.dart';
-import 'package:arthub_flutter/models/settings_model.dart';
-import 'package:arthub_flutter/providers/checkout_provider.dart';
-import './screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,25 +9,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => SettingsModel()),
-        ChangeNotifierProvider(create: (_) => CheckoutProvider()),
-      ],
-      child: MaterialApp(
-        title: 'ArtHub',
-        theme: ThemeData(
-          primaryColor: AppStyles.primaryColor,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: AppStyles.primaryColor,
-          ),
-          useMaterial3: true,
-          scaffoldBackgroundColor: Colors.white,
-        ),
-        // home: const MainScreen(),
-        home: const SplashScreen(),
-        debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: 'ArtHub',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(), 
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Hello, World!')),
     );
   }
 }
